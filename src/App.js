ed00e6154b4a9baba8ece474b0672e4a3ac763d0
spazdrum <spazdrum@gmail.com>
2020-11-22 23:38:02 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// import Header from "./components/header";
+// import Introduction from "./components/introduction";
+// import About from "./components/about";
+// import Projects from "./components/projects";
+// import Timeline from "./components/timeline";
+// import Contact from "./components/contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div id="page">
+        <div id="wrap">
+          {/* <Header></Header> */}
+          <div id="main">
+            {/* <Introduction></Introduction>
+            <About></About>
+            <Projects></Projects>
+            <Timeline></Timeline>
+            <Contact></Contact> */}
+          </div> 
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
